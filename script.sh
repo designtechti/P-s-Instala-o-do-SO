892f633343da1da80d345ce5b950f247620ab368
Design Tech TI <mrc_490@hotmail.com>
2021-05-04 09:31:21 -0300

    Alterado o código shell script

--- script.sh
+++ script.sh
@@ -10,9 +10,7 @@ sudo apt update &&
 
 ## Instalando pacotes e programas do repositório deb do Ubuntu ##
 
-sudo apt install dropbox -y &&
-sudo apt install teamviewer -y &&
-sudo apt install python3 python-pip wine nautilus-dropbox docker docker-compose git build-essential libssl-dev flatpak gnome-software-plugin-flatpak -y &&
+sudo apt install python3 python3-pip wine nautilus-dropbox docker docker-compose git build-essential libssl-dev flatpak gnome-software-plugin-flatpak winff -y &&
 
 
 ## Adicionando repositório Flathub ##
@@ -21,57 +19,70 @@ flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flat
 
 ## Instalando Apps do Flathub ##
 
-sudo flatpak install flathub cc.arduino.arduinoide -y &&
-sudo flatpak install flathub com.google.AndroidStudio -y &&
-sudo flatpak install flathub org.audacityteam.Audacity -y &&
-sudo flatpak install flathub com.rafaelmardojai.Blanket -y &&
-sudo flatpak install flathub org.blender.Blender -y &&
-sudo flatpak install flathub org.gnome.Boxes -y &&
-sudo flatpak install flathub org.gnome.Cheese -y &&
-sudo flatpak install flathub org.mozilla.Thunderbird -y &&
-sudo flatpak install flathub com.discordapp.Discord -y &&
-sudo flatpak install flathub org.electrum.electrum -y &&
-sudo flatpak install flathub org.filezillaproject.Filezilla -y &&
-sudo flatpak install flathub com.github.johnfactotum.Foliate -y &&
-sudo flatpak install flathub com.meetfranz.Franz -y &&
-sudo flatpak install flathub com.axosoft.GitKraken -y &&
-sudo flatpak install flathub org.inkscape.Inkscape -y &&
-sudo flatpak install flathub org.jamovi.jamovi -y &&
-sudo flatpak install flathub org.kde.kdenlive -y &&
-sudo flatpak install flathub net.codeindustry.MasterPDFEditor -y &&
-sudo flatpak install flathub org.apache.netbeans -y &&
-sudo flatpak install flathub com.obsproject.Studio -y &&
-sudo flatpak install flathub org.onlyoffice.desktopeditors -y &&
-sudo flatpak install flathub org.librehunt.Organizer -y &&
-sudo flatpak install flathub ch.openboard.OpenBoard -y &&
-sudo flatpak install flathub org.gimp.GIMP -y &&
-sudo flatpak install flathub com.jetbrains.Pycharm-Community -y &&
-sudo flatpak install flathub com.jetbrains.WebStorm -y &&
-sudo flatpak install flathub org.videolan.VLC -y &&
-sudo flatpak install flathub edu.mit.Scratch -y &&
-sudo flatpak install flathub com.skype.Client -y &&
-sudo flatpak install flathub com.slack.Slack -y &&
-sudo flatpak install flathub com.snes9x.Snes9x -y &&
-sudo flatpak install flathub com.spotify.Client -y &&
-sudo flatpak install flathub com.valvesoftware.Steam -y &&
-sudo flatpak install flathub com.sublimetext.three -y &&
-sudo flatpak install flathub org.telegram.desktop -y &&
-sudo flatpak install flathub com.transissionbt.Transmission -y &&
-sudo flatpak install flathub io.dbeaver.DbeaverCommunity -y &&
-sudo flatpak install flathub us.zoom.Zoom -y &&
+flatpak install flathub cc.arduino.arduinoide -y &&
+flatpak install flathub com.google.AndroidStudio -y &&
+flatpak install flathub org.audacityteam.Audacity -y &&
+flatpak install flathub com.rafaelmardojai.Blanket -y &&
+flatpak install flathub org.blender.Blender -y &&
+flatpak install flathub org.gnome.Boxes -y &&
+flatpak install flathub org.gnome.Cheese -y &&
+flatpak install flathub org.mozilla.Thunderbird -y &&
+flatpak install flathub com.discordapp.Discord -y &&
+flatpak install flathub org.electrum.electrum -y &&
+flatpak install flathub org.filezillaproject.Filezilla -y &&
+flatpak install flathub com.github.johnfactotum.Foliate -y &&
+flatpak install flathub com.meetfranz.Franz -y &&
+flatpak install flathub com.axosoft.GitKraken -y &&
+flatpak install flathub org.inkscape.Inkscape -y &&
+flatpak install flathub org.jamovi.jamovi -y &&
+flatpak install flathub org.darktable.Darktable -y &&
+flatpak install flathub org.kde.kdenlive -y &&
+flatpak install flathub org.kde.krita -y &&
+flatpak install flathub net.codeindustry.MasterPDFEditor -y &&
+flatpak install flathub org.apache.netbeans -y &&
+flatpak install flathub com.obsproject.Studio -y &&
+flatpak install flathub org.onlyoffice.desktopeditors -y &&
+flatpak install flathub org.librehunt.Organizer -y &&
+flatpak install flathub ch.openboard.OpenBoard -y &&
+flatpak install flathub org.gimp.GIMP -y &&
+flatpak install flathub com.jetbrains.Pycharm-Community -y &&
+flatpak install flathub com.jetbrains.WebStorm -y &&
+flatpak install flathub org.videolan.VLC -y &&
+flatpak install flathub edu.mit.Scratch -y &&
+flatpak install flathub com.skype.Client -y &&
+flatpak install flathub com.slack.Slack -y &&
+flatpak install flathub com.snes9x.Snes9x -y &&
+flatpak install flathub com.spotify.Client -y &&
+flatpak install flathub com.valvesoftware.Steam -y &&
+flatpak install flathub com.sublimetext.three -y &&
+flatpak install flathub org.telegram.desktop -y &&
+flatpak install flathub com.todoist.Todoist -y &&
+flatpak install flathub com.transissionbt.Transmission -y &&
+flatpak install flathub io.dbeaver.DbeaverCommunity -y &&
+flatpak install flathub us.zoom.Zoom -y &&
+flatpak install flathub com.jetbrains.IntelliJ-IDEA-Community &&
 
 ## Softwares que precisam de download externo ##
 
-cd ~/Downloads/ && 
+cd ~/Downloads/ || cd ~/Transferências/ && 
 
+wget -c https://download.anydesk.com/linux/anydesk_6.1.1-1_amd64.deb &&
+wget -c https://download.virtualbox.org/virtualbox/6.1.20/VirtualBox-6.1.20-143896-Linux_amd64.run &&
+wget -c https://download.teamviewer.com/download/linux/teamviewer_amd64.deb &&
+wget -c https://www.dropbox.com/download?dl=packages/ubuntu/dropbox_2020.03.04_amd64.deb &&
 wget -c https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb && 
 wget -c https://dl.google.com/dl/earth/client/current/google-earth-stable_current_amd64.deb &&
 wget -c https://uploads.treeunfe.me/downloads/instalar-freenfe.exe &&
-wget -c https://launchpad.net/rapid/pyqt/0.9.24/+download/install.py &&
-wget -c https://doc-0s-1g-docs.googleusercontent.com/docs/securesc/ha0ro937gcuc7l7deffksulhg5h7mbp1/0v83rmt4mij9897co9ufvor2r1jcj1am/1567965600000/07452089978596344616/*/12i-ihCDSZelx30-oNHJaKAzUei1etsbS?e=download && unzip 12i-ihCDSZelx30-oNHJaKAzUei1etsbS?e=download && cd "PHOTOGIMP V2018 - DIOLINUX" && cd "PATCH" && mkdir -p /home/$USER/.var/app/org.gimp.GIMP/config/GIMP/2.10/ && cp -R * /home/$USER/.var/app/org.gimp.GIMP/config/GIMP/2.10/ &&
+wget -c https://launchpad.net/rapid/pyqt/0.9.26/+download/install.py &&
+## wget -c https://sourceforge.net/projects/curlew/files/latest/download &&
+## wget -c https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64 &&
+wget -c https://github.com/Diolinux/PhotoGIMP/releases/download/1.0/PhotoGIMP.by.Diolinux.v2020.for.Flatpak.zip && 
 
+unzip *.zip && cd "PhotoGIMP by Diolinux v2020 for Flatpak" && cd "PATCH" && mkdir -p /home/$USER/.var/app/org.gimp.GIMP/config/GIMP/2.10/ && cp -R * /home/$USER/.var/app/org.gimp.GIMP/config/GIMP/2.10/ && ## Instala o PhotoGIMP para Flatpak
 sudo dpkg -i *.deb &&
 sudo python3 install.py && # Instala o Rapid Photo Downloader
+sudo ./VirtualBox-6.1.20-143896-Linux_amd64.run && 
+sudo tar -xvzf *.tar.gz &&
 
 ## Instalando pacotes Snap ##
  
@@ -84,6 +95,9 @@ sudo snap install notable --classic &&
 sudo snap install simplescreenrecorder &&
 sudo snap install vokoscreen-ng &&
 sudo snap install mysql-workbench-community &&
+sudo snap install chaseapp &&
+
+
 
 ## Wine softwares ###
 ## wget -c https://uploads.treeunfe.me/downloads/instalar-freenfe.exe
